b974a388e24183cb7733d333d3c668e02d9c6e5f
jxxviel-rgb <fiqriilhamsyah09@gmail.com>
2021-10-04 17:42:39 +0700

    change baseurl from http to https

--- src/config/baseurl.js
+++ src/config/baseurl.js
@@ -1,2 +1,2 @@
 import axios from "axios";
-export const baseUrlApi = axios.create({ baseURL: "http://management-madtive.himed.id/api" });
+export const baseUrlApi = axios.create({ baseURL: "https://management-madtive.himed.id/api" });
